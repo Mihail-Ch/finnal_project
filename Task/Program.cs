be2cf9e1b4a27ff7d8ab6a38e01f83c4eb4f671c
michail <mihail.chertov@gmail.com>
2022-12-26 23:07:52 +0300

    Решение задачи на C#

---
+++ Task/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array = new string[] {"hello", "swift", "C#", "box", "google", "gb", "Apple"};
+int index = 0;
+string[] newArray = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    string element = array[i];
+    if (element.Length <= 3) 
+    {
+        newArray[index] = element;
+        index++;
+}
+}
+Console.WriteLine(string.Join(Environment.NewLine, newArray));
+Console.ReadKey(true);
